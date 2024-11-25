07d57404c1d4616c5417d6446ea5793c338640ea
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-25 10:37:07 +0000

    ee

--- Dashboard.py
+++ Dashboard.py
@@ -19,7 +19,7 @@ df2['Site'] = df2['N° PCE'].map(mapping)
 df2 = df2.drop(columns=['N° PCE'])
 
 # Création de nouvelles colonnes pour l'année, le mois et le jour
-df2['Année'] = df2['Horodate'].dt.year  # Utilisation de dt.year pour extraire l'année comme entier
+df2['Année'] = df2['Horodate'].dt.year
 df2['Mois'] = df2['Horodate'].dt.month
 df2['Jour'] = df2['Horodate'].dt.day
 df2['Mois-Abrege'] = df2['Horodate'].dt.strftime('%b')  # Mois abrégés
@@ -74,6 +74,7 @@ if period_choice == 'Année':
         legend_title="Site"
     )
     st.plotly_chart(fig)
+
 elif period_choice == 'Mois':
     fig = go.Figure()
     for site in df_grouped['Site'].unique():
@@ -92,6 +93,7 @@ elif period_choice == 'Mois':
         legend_title="Site"
     )
     st.plotly_chart(fig)
+
 else:  # Par jour
     fig = go.Figure()
     for site in df_grouped['Site'].unique():
