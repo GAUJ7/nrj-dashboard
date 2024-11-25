cb4ea86d4a6f76669d9535c910de9d4094b96674
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-25 11:09:17 +0000

    pp

--- Dashboard.py
+++ Dashboard.py
@@ -104,7 +104,7 @@ df_filtered_no_date = df_filtered.drop(columns=['Date de relevé', 'Horodate', '
 df_filtered_no_date = df_filtered_no_date[['Site', 'Année', 'Mois', 'Jour', 'Energie consommée (kWh)']]
 
 df_filtered_no_date['Mois'] = df_filtered_no_date['Mois'].apply(lambda x: pd.to_datetime(f'2024-{x}-01').strftime('%B'))
-df_filtered_no_date['Année'] = df_filtered_no_date['Année'].astype(str).str.replace(',', '').astype(int)
+df_filtered_no_date['Année'] = pd.to_datetime(df_filtered_no_date['Année'], format='%Y').dt.year
 
 st.write(df_filtered_no_date)
 
