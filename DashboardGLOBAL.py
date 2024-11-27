b1c6a925673598d940bffba00ab0e2665eae7210
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-27 17:31:09 +0100

    rr

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -17,7 +17,7 @@ df2['Mois'] = df2['Date'].dt.month
 df2['Jour'] = df2['Date'].dt.date
 df2['Mois-Abrege'] = df2['Date'].dt.strftime('%b')  # Mois abrégés (ex: Jan, Feb, Mar, etc.)
 df2['Année-Mois'] = df2['Année'] * 100 + df2['Mois']
-df2['Semaine'] = df2['Date'].dt.isocalendar().week
+df2['Semaine'] = df2['Année'] * 100 + df2['Date'].dt.isocalendar().week
 
 df2 = df2[df2['Année'].isin([2023, 2024])]
 
