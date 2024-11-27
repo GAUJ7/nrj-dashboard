2df233dff11dd0ee38be76072037996386d35fc4
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-27 17:07:52 +0100

    cc

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -152,7 +152,7 @@ fig.update_layout(
     yaxis_title=f'Consommation ({energie_choice})',
     legend_title="Site",
     xaxis=dict(type='category', categoryorder='category ascending'),
-    width=1200,  # Largeur du graphique
+    width=1500,  # Largeur du graphique
     height=800,  # Hauteur du graphique
 )
 
