5fd6f633ded2e3b4a4b034d8eaebb992dee92c12
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-27 17:10:53 +0100

    ff

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -153,7 +153,7 @@ fig.update_layout(
     legend_title="Site",
     xaxis=dict(type='category', categoryorder='category ascending'),
     height=700,  # Hauteur du graphique
-    width=3000,  # Largeur du graphique
+    width=10000,  # Largeur du graphique
 )
 
 # Affichage du graphique dans Streamlit
