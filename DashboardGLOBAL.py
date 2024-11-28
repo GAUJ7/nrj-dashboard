3ecf5b6962b0705f967d4fff40e94d34932e152d
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-28 16:31:28 +0100

    qqq

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -150,8 +150,12 @@ for idx, site in enumerate(df_grouped['Site'].unique()):
             marker=dict(color=color)
         ))
     elif period_choice == 'Mois':
+        # Mise en forme de la semaine pour afficher mois et année (ex : 202301 -> Janvier 2023)
+        site_data['Mois'] = site_data['Mois'].apply(
+            lambda x: f"{pd.to_datetime(str(x), format='%Y%m').strftime('%B %Y')}"
+        )
         fig.add_trace(go.Bar(
-            x=site_data['Mois'],
+            x=site_data['Semaine'],
             y=site_data[energie_choice],
             name=site,
             marker=dict(color=color)
