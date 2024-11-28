78e4b7aa1d884f75123514ccb1e9930431e55aa9
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-28 16:34:29 +0100

    cc

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -203,7 +203,7 @@ fig.update_layout(
 # Affichage du graphique dans Streamlit
 if period_choice != 'Jour' and period_choice in df_grouped.columns:
     df_grouped[period_choice] = df_grouped[period_choice].apply(
-        lambda x: f"{pd.to_datetime(str(x), format='%Y%m').strftime('%B %Y')}" if len(str(x)) == 6 else x
+        lambda x: f"{pd.to_datetime(str(x), format='%Y%m').strftime('%B %Y')}" if period_choice == 'Mois'
     )
 
 if energie_choice in df_grouped.columns:
