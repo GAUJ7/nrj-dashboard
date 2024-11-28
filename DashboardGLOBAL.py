e4e0363129ecf04f2947a8ed24d50ea08c3e61e3
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-28 17:16:04 +0100

    xx

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -154,6 +154,9 @@ for idx, site in enumerate(df_grouped['Site'].unique()):
         site_data['Mois'] = site_data['Mois'].apply(
             lambda x: f"{pd.to_datetime(str(x), format='%Y%m').strftime('%B %Y')}" if period_choice == 'Mois' else x
         )
+            # Trier les données par Mois en ordre croissant
+        site_data = site_data.sort_values(by='Mois', ascending=True)
+        
         fig.add_trace(go.Bar(
             x=site_data['Mois'],
             y=site_data[energie_choice],
