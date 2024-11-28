7c20e502a60b95d3a9ac516cda6356429a0c674e
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-28 15:43:22 +0100

    xx

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -196,7 +196,11 @@ fig.update_layout(
 )
 
 # Affichage du graphique dans Streamlit
+if period_choice except 'Date' in df_grouped.columns:
+    df_grouped[period_choice] = df_grouped[period_choice].apply(lambda x: f"{x:,.0f}".replace(',', ''))
 
+if energie_choice in df_grouped.columns:
+    df_grouped[energie_choice] = df_grouped[energie_choice].apply(lambda x: f"{x:,.2f}".replace(',', ''))
 
 st.plotly_chart(fig)
 st.write(df_grouped)
