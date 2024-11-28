a4e500e52c232f8b19050f86f7403a176bf8a81d
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-28 17:18:02 +0100

    ww

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -151,12 +151,13 @@ for idx, site in enumerate(df_grouped['Site'].unique()):
         ))
     elif period_choice == 'Mois':
         # Mise en forme de la semaine pour afficher mois et année (ex : 202301 -> Janvier 2023)
+
+        site_data = site_data.sort_values(by='Mois', ascending=True)
+
         site_data['Mois'] = site_data['Mois'].apply(
             lambda x: f"{pd.to_datetime(str(x), format='%Y%m').strftime('%B %Y')}" if period_choice == 'Mois' else x
         )
-            # Trier les données par Mois en ordre croissant
-        site_data = site_data.sort_values(by='Mois', ascending=True)
-        
+
         fig.add_trace(go.Bar(
             x=site_data['Mois'],
             y=site_data[energie_choice],
