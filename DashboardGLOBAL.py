253a6b93f125fba5c391e59db9a0ffa92f78b98c
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-28 16:32:25 +0100

    cc

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -155,7 +155,7 @@ for idx, site in enumerate(df_grouped['Site'].unique()):
             lambda x: f"{pd.to_datetime(str(x), format='%Y%m').strftime('%B %Y')}" if len(str(x)) == 6 else x
         )
         fig.add_trace(go.Bar(
-            x=site_data['Semaine'],
+            x=site_data['Mois'],
             y=site_data[energie_choice],
             name=site,
             marker=dict(color=color)
