d03068bc800264e5c90a797fa496a8367323181f
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-29 11:02:33 +0100

    dd

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -183,7 +183,7 @@ for idx, site in enumerate(df_grouped['Site'].unique()):
         ))
     else:
         if energie_choice == 'Gaz (kWh/kg)':
-            site_data = site_data[site_data[energie_choice] < 100]
+            site_data = site_data[site_data[energie_choice] < 15]
 
         if energie_choice == 'Electricité (kWh/kg)':
             site_data = site_data[site_data[energie_choice] < 7]
