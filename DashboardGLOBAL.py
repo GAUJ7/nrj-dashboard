606127bc9e73b43183321c1381beb4f880a748d8
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-29 10:23:24 +0100

    vv

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -204,7 +204,6 @@ fig.update_layout(
         type='category',
         categoryorder='category ascending',
         tickfont=dict(size=16)  # Taille des labels des ticks de l'axe X
-        gridcolor='white'  # Change la couleur de la grille en blanc
     ),
     yaxis_title=f'Consommation ({energie_choice})',
     yaxis_title_font=dict(size=18),  # Taille du titre de l'axe Y
@@ -215,8 +214,6 @@ fig.update_layout(
     legend_title="Site",
     height=500,  # Hauteur du graphique
     width=2000,  # Largeur du graphique
-
-
 )
 
 # Affichage du graphique dans Streamlit
