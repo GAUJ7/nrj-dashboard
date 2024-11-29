a20e1716df7aef11ea3c1b9f4fb1919e1460e607
GAUJ7 <johnnygautier@hotmail.fr>
2024-11-29 10:30:49 +0100

    hh

--- DashboardGLOBAL.py
+++ DashboardGLOBAL.py
@@ -214,7 +214,7 @@ fig.update_layout(
         tickfont=dict(size=16),  # Taille des labels des ticks de l'axe Y
         showgrid=True,  # Afficher la grille
         gridcolor='white',  # Change la couleur de la grille en blan
-        linecolor='white'  # Change la couleur de l'axe Y en blanc
+        zerolinecolor='white',  # Change la couleur de la ligne zéro
     ),
     legend_title="Site",
     height=500,  # Hauteur du graphique
