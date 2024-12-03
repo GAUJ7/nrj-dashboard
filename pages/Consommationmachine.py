6d0d577ed060473287050a6966097e45aee0e95c
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-03 09:38:13 +0100

    vv

---
+++ pages/Consommationmachine.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import streamlit as st
+import plotly.express as px  # Pour accéder à des palettes de couleurs
+
+st.set_page_config(page_title="Consommation machine", layout="wide")
