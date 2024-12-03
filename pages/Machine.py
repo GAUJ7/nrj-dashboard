10853aa676f786441ed66974b439d86dbe84232b
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-03 11:22:05 +0100

    dd

--- pages/Machine.py
+++ pages/Machine.py
@@ -46,18 +46,16 @@ period_choice = st.sidebar.radio("Sélectionner la période", ('Année', 'Mois',
 
 # Filtrage des données par site
 if site_selection == 'Global':
-    # Sinon, grouper df2 par période et site, et sommer selon l'énergie choisie
+    # Si le site est 'Global', on groupe df2 par période et machine et on somme selon l'énergie choisie
     df_filtered = df2.groupby([period_choice, 'Machine'])[energie_choice].sum().reset_index()
-    # Sinon, filtrer df2 selon le site sélectionné
-else:    
+else:
     # Sinon, on filtre les données selon le site sélectionné
-    if machine_selection == 'Global':        
+    if machine_selection == 'Global':
         # Si l'option 'Global' est choisie pour la machine, on groupe par période, site, et machine
-        df_filtered = df2.groupby([site_selection, 'Machine'])[energie_choice].sum().reset_index() 
-        df_filtered = df2.groupby([period_choice, 'Machine'])[energie_choice].sum().reset_index() 
+        df_filtered = df2.groupby([period_choice, site_selection, 'Machine'])[energie_choice].sum().reset_index()
     else:
         # Si une machine spécifique est choisie, on filtre les données pour cette machine
-        df_filtered = df2[df2['Machine'] == machine_selection]
+        df_filtered = df2[(df2['Site'] == site_selection) & (df2['Machine'] == machine_selection)]
  
 # Filtrage selon la période choisie
 if period_choice == 'Année':
