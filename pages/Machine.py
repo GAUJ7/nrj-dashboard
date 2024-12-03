ce26166e220ae52409a4d5a97ad880b127648052
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-03 17:31:07 +0100

    ss

--- pages/Machine.py
+++ pages/Machine.py
@@ -79,8 +79,11 @@ else:
             df_filtered = df_filtered.groupby([period_choice, 'Machine'])[energie_choice].sum().reset_index()
             # Si l'option 'Global' est choisie pour la machine, on groupe par période, site, et machine
     else:
-        # Si une machine spécifique est choisie, on filtre les données pour cette machine
-        df_filtered = df2[(df2['Site'] == site_selection) & (df2['Machine'] == machine_selection)]
+        if energie_choice == 'Gaz (kWh/kg)':
+            df_final = df_final[(df_final['Site'] == site_selection) & (df_final['Machine'] == machine_selection)]
+        else:
+            # Si une machine spécifique est choisie, on filtre les données pour cette machine
+            df_filtered = df2[(df2['Site'] == site_selection) & (df2['Machine'] == machine_selection)]
  
 # Filtrage selon la période choisie
 if period_choice == 'Année':
