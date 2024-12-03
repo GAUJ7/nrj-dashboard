dadc511ebd8f5b271830ece3e8d3e0ed955cdab2
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-03 10:05:50 +0100

    ll

--- pages/Machine.py
+++ pages/Machine.py
@@ -4,4 +4,4 @@ import plotly.graph_objects as go
 import streamlit as st
 import plotly.express as px  # Pour accéder à des palettes de couleurs
 
-st.write('En cours de développement')
+st.write('En cours de développement!!')
