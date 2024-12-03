d468c19dc8a4b1d57a64129338cd60fd1be94668
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-03 09:42:11 +0100

    vv

--- pages/Machine_conso.py
+++ pages/Machine_conso.py
@@ -4,4 +4,4 @@ import plotly.graph_objects as go
 import streamlit as st
 import plotly.express as px  # Pour accéder à des palettes de couleurs
 
-st.set_page_config(page_title="Consommation machine", layout="wide")
+st.set_page_config(page_title="Machine_Conso", layout="wide")
