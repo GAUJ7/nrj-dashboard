8f0bf5a96cbaa87e390b63b1d607a0195dce4261
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-10 17:23:16 +0100

    ss

--- TableaudebordPTWEFR.py
+++ TableaudebordPTWEFR.py
@@ -119,16 +119,7 @@ if site_selection == 'Global' or 'Total':
         # Sinon, grouper df2 par période et site, et sommer selon l'énergie choisie
         df_filtered = df2.groupby([period_choice, 'Site'])[energie_choice].sum().reset_index()
         
-elif site_selection == 'Total' :
-
-    if energie_choice == 'Gaz (kWh/kg)' or energie_choice == 'Electricité (kWh/kg)':
-        # Si l'énergie choisie est 'Gaz (kWh/kg)' ou 'Electricité (kWh/kg)', filtrer df_final selon le site sélectionné
-        df_filtered = df_final
-    else:
-        # Sinon, filtrer df2 selon le site sélectionné
-        df_filtered = df2.groupby([period_choice, 'Site'])[energie_choice].sum().reset_index()
-
-elif energie_choice == 'Gaz (kWh/kg)' or energie_choice == 'Electricité (kWh/kg)' or energie_choice == 'Empreinte carbone (tCO2)':
+elif energie_choice == 'Gaz (kWh/kg)' or energie_choice == 'Electricité (kWh/kg)':
     # Si l'énergie choisie est 'Gaz (kWh/kg)' ou 'Electricité (kWh/kg)', filtrer df_final selon le site sélectionné
     df_filtered = df_final[df_final['Site'] == site_selection]
 
