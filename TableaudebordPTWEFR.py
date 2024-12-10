2a406e55d540c61b288c3f561d597fb284b85e7f
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-10 12:26:55 +0100

    xx

--- TableaudebordPTWEFR.py
+++ TableaudebordPTWEFR.py
@@ -3,6 +3,7 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 import plotly.express as px  # Pour accéder à des palettes de couleurs
+import toml
 
 st.set_page_config(page_title="Tableau", layout="wide")
 
