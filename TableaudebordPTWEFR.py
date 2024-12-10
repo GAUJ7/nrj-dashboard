85fdeda2ca9f417a047ec3a7858834234c17dd2c
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-10 17:00:37 +0100

    cc

--- TableaudebordPTWEFR.py
+++ TableaudebordPTWEFR.py
@@ -75,7 +75,7 @@ sites = df2['Site'].unique()
 site_selection = st.sidebar.selectbox('Choisissez un site', ['Global'] + list(sites) + ['Total'])
 
 # Choisir l'énergie à afficher
-energie_choice = st.sidebar.radio("Choisissez l'indicateur", ['Gaz (kWh/kg)','Empreinte carbone (tCO2)', 'Electricité (kWh/kg)', 'Gaz (kWh)', 'Electricité (kWh)', 'PE (kg)'])
+energie_choice = st.sidebar.radio("Choisissez l'indicateur", ['Gaz (kWh/kg)', 'Electricité (kWh/kg)','Empreinte carbone (tCO2)', 'Gaz (kWh)', 'Electricité (kWh)', 'PE (kg)'])
 
 # Choisir la période de filtrage
 period_choice = st.sidebar.radio("Sélectionner la période", ('Année', 'Mois','Semaine','Jour', ))
