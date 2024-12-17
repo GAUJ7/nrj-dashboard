ac74022b7c1252772c178a0d5f798ed27e3c15f9
GAUJ7 <johnnygautier@hotmail.fr>
2024-12-17 11:10:34 +0100

    tt

--- pages/Machine.py
+++ pages/Machine.py
@@ -4,6 +4,7 @@ import plotly.graph_objects as go
 import streamlit as st
 import plotly.express as px  # Pour accéder à des palettes de couleurs
 import toml
+from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
 
 # Fonction pour charger les informations d'authentification
